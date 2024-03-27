a2cf126840c554493dc6f4307e1178615fe9a14e
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-03-27 22:58:28 +0300

    solution isEvenNumber

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,7 @@
-// code
+let number1 = Math.floor(Math.random() * 100);
+
+function isEvenNumber(number) {
+    return number % 2 === 0 ? `${number}:Это четное число` : `${number}:Это нечетное число`
+}
+
+console.log(isEvenNumber(number1))
