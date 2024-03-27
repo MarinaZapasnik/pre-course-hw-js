bb9f892478d61f59ad31f22856876418f99882f6
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-03-27 23:12:14 +0300

    solution isEvenOrOdd

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,7 +1,7 @@
 let number1 = Math.floor(Math.random() * 100);
 
-function isEvenNumber(number) {
-    return number % 2 === 0 ? `${number}:Это четное число` : `${number}:Это нечетное число`
+function isEvenOrOdd(number) {
+    return number % 2 === 0 ? `${number} : Это четное число` : `${number} : Это нечетное число`
 }
 
-console.log(isEvenNumber(number1))
+console.log(isEvenOrOdd(number1))
